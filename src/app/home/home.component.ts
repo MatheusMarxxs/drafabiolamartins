a333ededc38a0920a7cb494ec796950e59cde147
MatheusMarxxs <matheus@marxxs.com.br>
2023-04-25 15:27:16 -0300

    Setup redirect

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  constructor() {
+    setTimeout(() => {
+      window.location.href = 'https://api.whatsapp.com/send/?phone=5513974109547&text=Ol%C3%A1%20Dra.%20Fab%C3%ADola!%20Eu%20gostaria%20de%20agendar%20uma%20avalia%C3%A7%C3%A3o.&type=phone_number&app_absent=0';
+    }, 3000);
+  }
 }
